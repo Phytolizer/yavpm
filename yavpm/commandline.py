05a297b0a9a0a26a510f5bdbb07ae0484a162b7d
Kyle Coffey <mariorules24@gmail.com>
2021-11-28 17:59:24 -0600

    formatting, isort

--- yavpm/commandline.py
+++ yavpm/commandline.py
@@ -1,6 +1,5 @@
 import commands
 
-
 ##takes the arguments provided from command line
 ##possibly just send in the list from main...
 ##no need to import 'sys' not sure yet.
--- yavpm/commands.py
+++ yavpm/commands.py
@@ -1,13 +1,16 @@
 from yavpm import git
 
+
 ##dump into git.clone()
 def add(project_name):
     pass
+
 
 ##possibly add git.pull() method
 ##update all projects installed
 def update(installed_list):
     pass
+
 
 ##take project_name and remove from installed
 ##update installed list of changes
