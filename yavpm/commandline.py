15b653b922bf8578b424cefdf4da0a5a7ea10470
Kyle Coffey <mariorules24@gmail.com>
2021-12-03 19:30:46 -0600

    Update commandline.py
    Discussion ongoing about imports here

--- yavpm/commandline.py
+++ yavpm/commandline.py
@@ -1,5 +1,9 @@
 from yavpm import commands
+# Contains all the command-line arguments.
+# TODO(gloat): Do we process the args beforehand?
+#  If so, they must be passed in, otherwise we can
+#  use the below import.
+from sys import argv
 
 ##takes the arguments provided from command line
 ##possibly just send in the list from main...
-##no need to import 'sys' not sure yet.
